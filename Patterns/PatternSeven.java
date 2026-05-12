4c694f0975d6b48690b6f9bc9c761da12e9410dd
VemulaMaruthi <maruthivemula95@gmail.com>
2026-05-12 12:35:02 +0530

    implementated pattern seven

--- Patterns/PatternSeven.java
+++ Patterns/PatternSeven.java
@@ -9,13 +9,13 @@ public class PatternSeven {
         int rows = sc.nextInt();
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < rows-i-1; j++) {
-                System.out.print("| ");
+                System.out.print(" ");
             }
             for (int j = 0; j < 2*i+1; j++) {
                 System.out.print("*");
             }
             for (int j = 0; j < rows-i-1; j++) {
-                System.out.print("|");
+                System.out.print(" ");
             }
             System.out.println();
         }
