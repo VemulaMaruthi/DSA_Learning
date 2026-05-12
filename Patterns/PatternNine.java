d1eafeba44f9d7684353fde2ea632a162f5f1608
VemulaMaruthi <maruthivemula95@gmail.com>
2026-05-12 12:48:38 +0530

    implementated pattern eight

---
+++ Patterns/PatternNine.java
@@ -0,0 +1,36 @@
+package Patterns;
+
+import java.util.Scanner;
+
+public class PatternNine {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of rows = ");
+        int rows = sc.nextInt();
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < rows-i-1; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 2*i+1; j++) {
+                System.out.print("*");
+            }
+            for (int j = 0; j < rows-i-1; j++) {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+        for (int i = rows-2; i >= 0 ; i--) {
+            for (int j = rows-i-1; j > 0 ; j--) {
+                System.out.print(" ");
+            }
+            for (int j = 2*i+1; j > 0 ; j--) {
+                System.out.print("*");
+            }
+            for (int j = rows-i-1; j > 0 ; j--) {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
