7d299abdb23002160f06ac78a4c6bdc0338a46b8
VemulaMaruthi <maruthivemula95@gmail.com>
2026-05-12 11:24:47 +0530

    implementated pattern three

--- Patterns/PatternThree.java
+++ Patterns/PatternThree.java
@@ -2,14 +2,14 @@ package Patterns;
 
 import java.util.Scanner;
 
-public class PatternTwo {
+public class PatternThree {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the number of rows = ");
         int rows = sc.nextInt();
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j <= i; j++) {
-                System.out.print("* ");
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= i ; j++) {
+                System.out.print(j);
             }
             System.out.println();
         }
