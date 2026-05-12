1fa927802b675220b440290e2f9536f9b142438d
VemulaMaruthi <maruthivemula95@gmail.com>
2026-05-12 12:33:59 +0530

    implementated pattern seven

---
+++ Patterns/PatternSeven.java
@@ -0,0 +1,24 @@
+package Patterns;
+
+import java.util.Scanner;
+
+public class PatternSeven {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of rows = ");
+        int rows = sc.nextInt();
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < rows-i-1; j++) {
+                System.out.print("| ");
+            }
+            for (int j = 0; j < 2*i+1; j++) {
+                System.out.print("*");
+            }
+            for (int j = 0; j < rows-i-1; j++) {
+                System.out.print("|");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
